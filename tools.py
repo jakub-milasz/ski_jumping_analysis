16250dfb88803bf0eaf49710a2b3617d5e809d8f
Jakub Milasz <jakubmilasz@gmail.com>
2025-02-07 12:47:32 +0100

    Small changes

--- tools.py
+++ tools.py
@@ -31,9 +31,10 @@ def analyse_skijumper(hill, name):
     res = data[data['codex_x'] == find_codex(name)]
     res = res[['dist', 'k-point', 'codex_y', 'place', 'hill_size_x', 'note_points']]
     # res = res.copy()
+    # coefficient = distance / k-point
     res['coef'] = res['dist'] / res['k-point']
-    res['hs-jumps'] = res['dist'] >= res['hill_size_x']
     grouped_coefs = res.groupby('place')['coef'].mean()
+    # place with the highest average coefficient
     fav_hill = grouped_coefs.idxmax()
     return fav_hill
 
@@ -41,12 +42,15 @@ def analyse_skijumper(hill, name):
 def analyse_overall_stats(name):
     res = merged[merged['codex_x'] == find_codex(name)]
     res = res[['dist', 'k-point', 'note_points', 'hill_size_x', 'wind_comp']]
+    # jumps at least as long as the hill size
     hs_jumps = np.sum(res['dist'] >= res['hill_size_x'])
     mean_notes = res['note_points'].mean()
+    # average wind compensation
     average_comp = res['wind_comp'].mean()
     return hs_jumps, mean_notes, average_comp
 
 
+# plot histogram of distances
 def plot_histogram(name, dist):
     plt.hist(dist, bins=20)
     plt.title(f"Rozkład odległości uzyskanych przez skoczka {name}")
